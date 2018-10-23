672cf5a33da03f0314e2a13fb485fc6fa719fc13
80010-37-15 <80010-37-15@exchange.ad.afpanet>
2018-10-23 10:07:44 +0200

    .

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: 'leqxd777',
     database: 'electron'
 });
 // Connexion à la base de données
